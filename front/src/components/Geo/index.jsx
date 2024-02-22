3cc835a9296b8b0d3ae21c8df42e26ef99b25327
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2024-02-22 09:35:37 +0100

    migrate map to react

--- front/src/components/Geo/index.jsx
+++ front/src/components/Geo/index.jsx
@@ -1,10 +1,15 @@
-import './geo.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import './geo.css';
 
 const Geo = () => {
   return (
-    <div></div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 }
 
