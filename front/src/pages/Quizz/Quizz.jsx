a47b99ed1922342f705d55d1ff15d7d916a6a516
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2024-02-26 23:31:30 +0100

    fetch quizz from database

--- front/src/pages/Quizz/Quizz.jsx
+++ front/src/pages/Quizz/Quizz.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
 import './quizz.scss';
 
 const Quizz = () => {
+  const [quizz, setQuizz] = useState([]);
+
+  const fetchQuizz = async() => {
+    const response = await fetch(`http://localhost:4000/api/quizz`);
+    console.log(response);
+    const quizz = await response.json();
+    setQuizz(quizz);
+  }
+
+  fetchQuizz();
+
   return (
-    <div>quizz</div>
+    <div>
+      <h1>{quizz.name}</h1>
+      {
+        quizz.questions.map((item) => (
+          <div key={item.label}>
+            <label>{ item.question }</label>
+            <input type="text" />
+          </div>
+        ))
+      }
+
+      <div>
+        <label>Quelle est la couleur du chien</label>
+        <input type="text" />
+      </div>
+    </div>
   );
 }
 
