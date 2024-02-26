f5650f08116b914db0e63df4995aa1cef4fa5957
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2024-02-26 23:31:18 +0100

    fetch teams from database

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,32 +1,29 @@
 
+import { useEffect, useState } from 'react';
+
 import Geo from '../../components/Geo/Geo';
 import Profile from '../../components/Profile/Profile';
 import Ranking from '../../components/Ranking/Ranking';
 
-const teams = [
-  {
-    name: 'Team 1',
-    points: 235
-  },
-  {
-    name: 'Team 2',
-    points: 126
-  },
-  {
-    name: 'Team 3',
-    points: 50
-  },
-  {
-    name: 'Team 4',
-    points: 20
-  }
-];
-
 const Home = () => {
+  const [teams, setTeams] = useState([]);
+
+  const fetchTeams = async() => {
+    const response = await fetch('http://localhost:4000/api/teams');
+    const teams = await response.json();
+    setTeams(teams);
+  }
+
+  useEffect(() => {
+    fetchTeams();
+  }, []);
+
   return (
     <div>
       <Geo/>
-      <Profile team={teams[0]} />
+      {
+        teams.length > 0 && <Profile team={teams[0]} />
+      }
       <Ranking teams={teams} />
     </div>
   );
