a7279df06b24900e644b6ccc8732886e7a0ca0f7
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2024-02-26 23:30:53 +0100

    fix quizz model

--- back/models/quizz.js
+++ back/models/quizz.js
@@ -1,13 +1,11 @@
 const mongoose = require ('mongoose');
 
 module.exports = mongoose.model('Quizz', {
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  imageUrl: String,
-  quizz: [
+  name: String,
+  questions: [
     {
-      questionId: { type: String, required: true },
-      response:  String
+      label: { type: String, required: true },
+      anwser: { type: String, required: true }
     }
   ]
 });
--- back/models/team.js
+++ back/models/team.js
@@ -4,6 +4,7 @@ module.exports = mongoose.model('Team', {
   name: { type: String, required: true },
   password: { type: String, required: true },
   imageUrl: String,
+  points: Number,
   quizz: [
     {
       questionId: { type: String, required: true },
