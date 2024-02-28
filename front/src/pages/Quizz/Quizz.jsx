6d038868aa1fac4ff963b020a577660dfa03fa2f
alexandreLavenant <alexandre.lavenant@sekoia.fr>
2024-02-28 23:25:41 +0100

    init save quizz

--- front/src/pages/Quizz/Quizz.jsx
+++ front/src/pages/Quizz/Quizz.jsx
@@ -17,6 +17,15 @@ const defaultQuizz = {
 
 const Quizz = () => {
   const [quizz, setQuizz] = useState(defaultQuizz);
+  // const [team, setTeam] = useState({});
+
+  const save = async() => {
+    try {
+      await fetch('http://localhost:4000/api/teams/dzdadza', { method: 'PUT' });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   const fetchQuizz = async() => {
     try {
@@ -30,21 +39,35 @@ const Quizz = () => {
     }
   }
 
+  // const fetchTeam = async() => {
+  //   try {
+  //     const response = await fetch(`http://localhost:4000/api/teams/`);
+  //     const teams = await response.json();
+  //     setTeam(teams);
+  //     console.log(team);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // fetchTeam();
   fetchQuizz();
 
   return (
-    <div>
-      <h1>{quizz.name}</h1>
-      {
-        quizz.questions.map((item) => (
-          <div key={item.label}>
-            <label>{ item.label }</label>
-            <input type="text" />
-          </div>
-        ))
-      }
+    <div className="container flex flex-column center">
+      <form className="quizz">
+        <h1 className='mb'>{quizz.name}</h1>
+        {
+          quizz.questions.map((item) => (
+            <div key={item.label} className='flex flex-column mb-md'>
+              <label>{ item.label }</label>
+              <input type="text" className='mt-sm'/>
+            </div>
+          ))
+        }
 
-      <button type="button">Sauvegarder les réponses</button>
+        <button type="button" onClick={save}>Sauvegarder les réponses</button>
+      </form>
     </div>
   );
 }
